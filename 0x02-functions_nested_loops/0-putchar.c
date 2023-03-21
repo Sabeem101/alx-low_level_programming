c91daf9e230435868f6a5fe80c7223fde7759633
Sabeem101 <sabeemmouqere@gmail.com>
2023-03-21 18:39:22 +0000

    main.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Prints _putchar.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	_putchar string[]="_putchar";
+		int  ch;
+
+		for (ch = 0, ch < 8, ch++)
+			_putchar(string[ch]);
+		return 0;
+}
