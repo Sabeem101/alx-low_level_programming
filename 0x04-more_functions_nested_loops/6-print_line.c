1f4b1d6336318bdbaeba3aa8da7e73986387c355
Sabeem101 <sabeemmouqere@gmail.com>
2023-03-23 19:41:36 +0000

    lines - minor update

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * print_line - draws a line in the terminal.
- * 		followed by a new line.
+ * print_line - prints a line in the terminal.
+ * followed by a new line.
  * @n: input integer
- *
  * Return: Always 0
  */
 void print_line(int n)
 {
 	int i = 0;
 
-	while (i < n && n > 0)
+	if (n > 0)
 	{
-		_putchar('_');
-		i++;
+		for (; i < n; i++)
+			_putchar('_');
 	}
 	_putchar('\n');
 }
