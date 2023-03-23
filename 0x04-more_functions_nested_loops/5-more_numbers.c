171248c7b3ad1ead883be4614c2fe65ea7125369
Sabeem101 <sabeemmouqere@gmail.com>
2023-03-23 18:58:39 +0000

    Numbersx10 - minor update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,17 +6,18 @@
  *
  * Return: void.
  */
+
 void more_numbers(void)
 {
-	int t, n;
+	int i, n;
 
-	for (t = 0; t < 10; t++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (n = 0; n < 15; n++)
+		for (n = 0; n <= 14; n++)
 		{
 			if (n >= 10)
-				_putchar((n / 10) + 48);
-			_putchar((n % 10) + 48);
+				_putchar('1');
+			_putchar(n % 10 + '0');
 		}
 		_putchar('\n');
 	}
