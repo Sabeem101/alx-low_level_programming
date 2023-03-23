bad4f8b592138fb0019f8ae769b421fcdfda7578
Sabeem101 <sabeemmouqere@gmail.com>
2023-03-23 19:34:16 +0000

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal.
+ * 		followed by a new line.
+ * @n: an input integer
+ * Return: Always 0
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i < n && n > 0)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
