c6906dd9d4f750d77e065e9dce371c3de73071c7
Sabeem101 <sabeemmouqere@gmail.com>
2023-03-27 13:51:20 +0000

    String length counter

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - checks the lenght of a string
+ * @s: string to check
+ *
+ * @Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count])
+		count++;
+	return (count);
+}
