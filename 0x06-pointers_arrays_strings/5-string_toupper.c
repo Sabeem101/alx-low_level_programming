eed42e44f84f63292a522c1250167b78a5e5093b
Sabeem101 <sabeemmouqere@gmail.com>
2023-03-30 23:41:31 +0000

    to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - change all lowercase letters to uppercase
+ * @c: string
+ * Return: c
+ */
+
+char *string_toupper(char *c)
+{
+	int a;
+
+	for (a = 0; c[a] != '\0'; a++)
+	{
+		if (c[a] > 96 && c[a] < 123)
+		{
+			c[a] -= 32;
+		}
+	}
+	return (c);
+}
