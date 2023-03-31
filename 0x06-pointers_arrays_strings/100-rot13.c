b7ccb21ca7f8a767c240f6318566bad96220b047
Sabeem101 <sabeemmouqere@gmail.com>
2023-03-31 02:37:28 +0000

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a  string into rot13
+ * @s: string to encode
+ *
+ * Return: string address
+ */
+
+char *rot13(char *s)
+{
+	int c, d;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		for (d = 0; a[d] != '\0'; d++)
+		{
+			if (s[c] == a[d])
+			{
+				s[c] = b [d];
+				break;
+			}
+		}
+	}
+	return (s);
+}
