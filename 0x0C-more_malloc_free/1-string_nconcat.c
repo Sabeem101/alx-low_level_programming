8bf6960e5ef5375236d10ed3f34374460a3a705b
Sabeem101 <sabeemmouqere@gmail.com>
2023-04-14 02:12:45 +0000

    Concatenates two strings - minor update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,38 +12,38 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-        unsigned int y, o, sz1, sz2;
-        char *x;
+	unsigned int y, o, sz1, sz2;
+	char *x;
 
-        if (s1 == NULL)
-                s1= "";
+	if (s1 == NULL)
+		s1= "";
 
-        if (s2 == NULL)
-                s2= "";
+	if (s2 == NULL)
+		s2= "";
 
-        for (sz1 = 0; s1[sz1] != '\0'; sz1++)
-                ;
+	for (sz1 = 0; s1[sz1] != '\0'; sz1++)
+		;
 
-        for (sz2 = 0; s2[sz2] != '\0'; sz2++)
-                ;
+	for (sz2 = 0; s2[sz2] != '\0'; sz2++)
+		;
 
-        if (n > sz2)
-                n = sz2;
-        o = sz1 + n;
-        x = malloc((o + 1) * sizeof(char));
+	if (n > sz2)
+		n = sz2;
+	o = sz1 + n;
+	x = malloc((o + 1) * sizeof(char));
 
-        if (x == NULL)
-                return (NULL);
-        for( y = 0; y < o; y++)
-                if(y < sz1)
-                {
-                        x[y] = s1[y];
-                }
-                else
-                {
-                        x[y] = s2[y - sz1];
-                }
-        x[y] = '\0';
+	if (x == NULL)
+		return (NULL);
+	for( y = 0; y < o; y++)
+		if(y < sz1)
+		{
+			x[y] = s1[y];
+		}
+		else
+		{
+			x[y] = s2[y - sz1];
+		}
+	x[y] = '\0';
 
-        return (x);
+	return (x);
 }
