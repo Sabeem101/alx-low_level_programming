e4d60cc0c4b7a24ca4a8c2426bbf99e6c3a28aeb
Sabeem101 <sabeemmouqere@gmail.com>
2023-04-18 02:36:00 +0000

    new dog - minor update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,38 +1,5 @@
-#include <stdlib.h>
 #include "dog.h"
-
-char *_doggo_copy(char *d);
-
-/**
- * _doggo_copy - makes copy of an argument
- * @d: data to make a copy from
- *
- * Return: copy
- */
-
-char *_doggo_copy(char *d)
-{
-	int x, y;
-	char *z;
-
-	if (d == NULL)
-	{
-		return (NULL);
-	}
-	for (x = 0; d[x] != '\0'; x++)
-		;
-	z = malloc(sizeof(char) * (x + 1));
-	if (z == NULL)
-	{
-		return (NULL);
-	}
-	for (y = 0; d[x] != '\0'; y++)
-	{
-		z[y] = d[y];
-	}
-	z[y] = '\0';
-	return (z);
-}
+#include <stdlib.h>
 
 /**
  * new_dog - creates a new dog
@@ -40,40 +7,41 @@ char *_doggo_copy(char *d)
  * @age: dog age
  * @owner: dog owner
  *
- * Return: new dog
+ * Return: struct dog.
+ *          if fails, returns NULL.
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggo;
-	char *doggo_name, *doggo_owner;
+	int x, doggo_name, doggo_owner;
 
-	if (name == NULL || owner == NULL)
-	{
-		return (NULL);
-	}
-	doggo = malloc(sizeof(dog_t));
-	if (doggo == NULL)
-	{
-		return (NULL);
-	}
-	doggo_name = _doggo_copy(name);
-	if (doggo_name == NULL)
+	doggo = malloc(sizeof(doggo));
+	if (doggo == NULL || !(name) || !(owner))
 	{
 		free(doggo);
 		return (NULL);
 	}
-	(*doggo).name = name;
+	for (doggo_name = 0; name[doggo_name]; doggo_name++)
+		;
+	for (doggo_owner = 0; owner[doggo_owner]; doggo_owner++)
+		;
 
-	(*doggo).age = age;
+	doggo->name = malloc(doggo_name + 1);
+	doggo->owner = malloc(doggo_owner + 1);
 
-	doggo_owner = _doggo_copy(owner);
-	if (doggo_owner == NULL)
+	if (!(doggo->name) || !(doggo->owner))
 	{
-		free((*doggo).name);
+		free(doggo->owner);
+		free(doggo->name);
 		free(doggo);
 		return (NULL);
 	}
-	(*doggo).owner = doggo_owner;
+	for (x = 0; x < doggo_name; x++)
+		doggo->name[x] = name[x];
+	doggo->name[x] = '\0';
+	doggo->age = age;
+	for (x = 0; x < doggo_owner; x++)
+		doggo->owner[x] = owner[x];
+	doggo->owner[x] = '\0';
 	return (doggo);
 }
