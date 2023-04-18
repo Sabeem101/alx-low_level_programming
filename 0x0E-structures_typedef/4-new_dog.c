9e636d6bc085662541c5164259f5a26804107426
Sabeem101 <sabeemmouqere@gmail.com>
2023-04-18 02:44:22 +0000

    new dog - minor update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -38,8 +38,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for (x = 0; x < doggo_name; x++)
 		doggo->name[x] = name[x];
-
 	doggo->name[x] = '\0';
+
 	doggo->age = age;
 
 	for (x = 0; x < doggo_owner; x++)
