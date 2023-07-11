39a6540538f9dc8165567f962078dbad82f79cbd
Sabeem101 <sabeemmouqere@gmail.com>
2023-07-11 07:23:54 -0700

    Task 1 - minor update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 		{
 			return (-1);
 		}
-		return (-1);
+		return (1);
 	}
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
@@ -39,8 +39,8 @@ int create_file(const char *filename, char *text_content)
 	if (wr == -1)
 	{
 		close(fd);
-		return (-1);
+		return (1);
 	}
 	close(fd);
-	return (-1);
+	return (1);
 }
