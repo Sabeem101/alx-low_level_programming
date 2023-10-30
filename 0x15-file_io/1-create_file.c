7a67a255537bb022e5cb5cd821bc0e3d6ab7f53c
Sabeem101 <sabeemmouqere@gmail.com>
2023-10-30 04:56:13 -0700

    File I/O

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: file name
+ * @text_content: file content
+ *
+ * Return: 1 on success, -1 if failed.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wr;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_CREAT | O_WRONLY);
+		if (fd == -1)
+		{
+			return (-1);
+		}
+		return (1);
+	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	wr = write(fd, text_content, strlen(text_content));
+
+	if (wr == -1)
+	{
+		close(fd);
+		return (1);
+	}
+	close(fd);
+	return (1);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: file name
+ * @text_content: added content to file
+ *
+ * Return: 1 on success, -1 if failed
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, wr, nletters = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_APPEND | O_WRONLY);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+	while (text_content[nletters] != '\0')
+	{
+		nletters = nletters + 1;
+	}
+	wr = write(fd, text_content, nletters);
+
+	if (wr < 1)
+	{
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
