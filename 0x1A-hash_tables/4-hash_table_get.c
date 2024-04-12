3b5e024b71cd509839bbc454a4c29e4149adbea8
Sabeem101 <sabeemmouqere@gmail.com>
2024-04-12 02:49:53 -0700

    Betty fix

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,7 +1,8 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - retrieves a value associated with a key from the hash table.
+ * hash_table_get - retrieves a value associated
+ *	with a key from the hash table.
  * @ht: hash table to search
  * @key: key to find
  *
@@ -12,7 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int idx = 0;
 	hash_node_t *el;
 
-	if (ht == NULL || ht->size == 0 || ht->array == NULL||
+	if (ht == NULL || ht->size == 0 || ht->array == NULL ||
 		       key == NULL || strlen(key) == 0)
 	{
 		return (NULL);
